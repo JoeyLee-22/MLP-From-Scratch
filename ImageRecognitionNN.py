c2796e132a843abd4a90f5ca4ba345746cc82133
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-08-12 21:30:02 -0700

    Initial commit

---
+++ ImageRecognitionNN.py
@@ -0,0 +1,237 @@
+import time
+import random
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+from progress.bar import IncrementalBar
+
+class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+class NeuralNetwork():
+
+    width = 40
+    epochs = 5
+    numUsing = 60000
+    bs = 250
+    beta1 = 0.9
+    beta2 = 0.999
+    epsilon = 1e-8
+    learningRate = 0.001
+    Lambda = 0
+
+    def __init__(self, sizes, optimizer, activation): 
+        self.dimensions = sizes
+
+        self.optimizer = optimizer
+        self.activation = activation
+
+        self.x = np.arange(1,((self.numUsing/self.bs)*self.epochs)+1)
+        self.y = np.empty(int(((self.numUsing/self.bs)*self.epochs)))
+
+        self.secondLayerNeurons = np.empty(sizes[1])
+        self.outputNeurons = np.empty(sizes[2])
+
+        self.w1 = np.random.rand(sizes[1], sizes[0]) * 2 - 1
+        self.w2 = np.random.rand(sizes[2], sizes[1]) * 2 - 1
+        self.b1 = np.random.rand(sizes[1]) * 2 - 1
+        self.b2 = np.random.rand(sizes[2]) * 2 - 1
+
+        self.dw1 = np.zeros([sizes[1], sizes[0]])
+        self.dw2 = np.zeros([sizes[2], sizes[1]])
+        self.db1 = np.zeros([sizes[1]])
+        self.db2 = np.zeros([sizes[2]])
+
+        self.V_dw1 = np.zeros([sizes[1], sizes[0]])
+        self.V_dw2 = np.zeros([sizes[2], sizes[1]])
+        self.V_db1 = np.zeros([sizes[1]])
+        self.V_db2 = np.zeros([sizes[2]])
+
+        self.S_dw1 = np.zeros([sizes[1], sizes[0]])
+        self.S_dw2 = np.zeros([sizes[2], sizes[1]])
+        self.S_db1 = np.zeros([sizes[1]])
+        self.S_db2 = np.zeros([sizes[2]])
+
+        self.V_dw1_correct = np.empty([sizes[1], sizes[0]])
+        self.V_dw2_correct = np.empty([sizes[2], sizes[1]])
+        self.V_db1_correct = np.empty(sizes[1])
+        self.V_db2_correct = np.empty(sizes[2])
+
+        self.S_db2_correct = self.S_db2/(1-np.power(self.beta2, self.bs))
+        self.S_dw2_correct = self.S_dw2/(1-np.power(self.beta2, self.bs))
+        self.S_db1_correct = self.S_db1/(1-np.power(self.beta2, self.bs))
+        self.S_dw1_correct = self.S_dw1/(1-np.power(self.beta2, self.bs))
+
+        self.hiddenLayerErrors = np.empty(sizes[1])
+        self.outputLayerErrors = np.empty(sizes[2])
+
+    def sigmoid(self, x):
+        warnings.filterwarnings("ignore")
+        return 1/(1+np.exp(-x))
+
+    def sigmoidDerivative(self, x):
+        return np.multiply(x,(1-x))
+
+    def relu(self, x):
+	    return max(0.0, x)
+    
+    def reluDerivative(self, x):
+        return 1 * (x > 0)
+
+    def forwardProp(self, inputs):
+        if self.activation == 'sigmoid':
+            for i in range (self.dimensions[1]):
+                self.secondLayerNeurons[i] = self.sigmoid(np.dot(self.w1[i], inputs)+self.b1[i])
+        elif self.activation == 'relu':
+            for i in range (self.dimensions[1]):
+                self.secondLayerNeurons[i] = self.relu(np.dot(self.w1[i], inputs)+self.b1[i])
+
+        for i in range (self.dimensions[2]):
+            self.outputNeurons[i] = self.sigmoid(np.dot(self.w2[i], self.secondLayerNeurons)+self.b2[i])
+
+    def backProp(self, inputs, correct_output):
+        self.outputLayerErrors = np.subtract(self.outputNeurons, correct_output)
+        if self.activation == 'sigmoid':
+            self.hiddenLayerErrors = np.multiply(np.dot(self.w2.T, self.outputLayerErrors), self.sigmoidDerivative(self.secondLayerNeurons))
+        elif self.activation == 'relu':
+            self.hiddenLayerErrors = np.multiply(np.dot(self.w2.T, self.outputLayerErrors), self.reluDerivative(self.secondLayerNeurons))
+
+        self.db2 += self.outputLayerErrors
+        self.dw2 += np.outer(self.outputLayerErrors, self.secondLayerNeurons)
+
+        self.db1 += self.hiddenLayerErrors
+        self.dw1 += np.outer(self.hiddenLayerErrors, inputs)
+
+    def change(self):
+        if self.optimizer == 'vanilla':
+            self.b2 -= self.learningRate * self.db2
+            self.w2 -= self.learningRate * (self.dw2 + self.Lambda * self.w2)
+            self.b1 -= self.learningRate * self.db1
+            self.w1 -= self.learningRate * (self.dw1 + self.Lambda * self.w1)
+
+        elif self.optimizer == 'momentum':
+            self.V_db2 = self.beta1*self.V_db2 + (1-self.beta1)*self.db2
+            self.V_dw2 = self.beta1*self.V_dw2 + (1-self.beta1)*self.dw2
+            self.V_db1 = self.beta1*self.V_db1 + (1-self.beta1)*self.db1
+            self.V_dw1 = self.beta1*self.V_dw1 + (1-self.beta1)*self.dw1
+
+            self.b2 -= self.learningRate * self.V_db2
+            self.w2 -= self.learningRate * (self.V_dw2 + self.Lambda * self.w2)
+            self.b1 -= self.learningRate * self.V_db1
+            self.w1 -= self.learningRate * (self.V_dw1 + self.Lambda * self.w1)
+        
+        elif self.optimizer == 'adam':
+            self.V_db2 = self.beta1*self.V_db2 + (1-self.beta1)*self.db2
+            self.V_dw2 = self.beta1*self.V_dw2 + (1-self.beta1)*self.dw2
+            self.V_db1 = self.beta1*self.V_db1 + (1-self.beta1)*self.db1
+            self.V_dw1 = self.beta1*self.V_dw1 + (1-self.beta1)*self.dw1
+
+            self.S_db2 = self.beta2*self.S_db2 + (1-self.beta2)*np.square(self.db2)
+            self.S_dw2 = self.beta2*self.S_dw2 + (1-self.beta2)*np.square(self.dw2)
+            self.S_db1 = self.beta2*self.S_db1 + (1-self.beta2)*np.square(self.db1)
+            self.S_dw1 = self.beta2*self.S_dw1 + (1-self.beta2)*np.square(self.dw1)
+
+
+            self.V_db2_correct = self.V_db2/(1-np.power(self.beta1, self.bs))
+            self.V_dw2_correct = self.V_dw2/(1-np.power(self.beta1, self.bs))
+            self.V_db1_correct = self.V_db1/(1-np.power(self.beta1, self.bs))
+            self.V_dw1_correct = self.V_dw1/(1-np.power(self.beta1, self.bs))
+
+            self.S_db2_correct = self.S_db2/(1-np.power(self.beta2, self.bs))
+            self.S_dw2_correct = self.S_dw2/(1-np.power(self.beta2, self.bs))
+            self.S_db1_correct = self.S_db1/(1-np.power(self.beta2, self.bs))
+            self.S_dw1_correct = self.S_dw1/(1-np.power(self.beta2, self.bs))
+
+
+            self.b2 -= self.learningRate * (self.V_db2_correct/(np.sqrt(self.S_db2_correct)+self.epsilon))
+            self.w2 -= self.learningRate * (self.V_dw2_correct/(np.sqrt(self.S_dw2_correct)+self.epsilon))
+            self.b1 -= self.learningRate * (self.V_db1_correct/(np.sqrt(self.S_db1_correct)+self.epsilon))
+            self.w1 -= self.learningRate * (self.V_dw1_correct/(np.sqrt(self.S_dw1_correct)+self.epsilon))
+
+        self.dw1 = np.zeros([self.dimensions[1], self.dimensions[0]])
+        self.dw2 = np.zeros([self.dimensions[2], self.dimensions[1]])
+        self.db1 = np.zeros(self.dimensions[1])
+        self.db2 = np.zeros(self.dimensions[2])
+
+    def train(self, trainImages, trainLabels):
+        size = str(self.bs)
+        err_sum = 0.0
+        err_count = 0
+        avg_err = 0.0
+        correct = 0
+
+        batch_start_time = time.time()
+        for m in range (self.bs):
+            correct_output = np.zeros([self.dimensions[2]])
+            correct_output[trainLabels[m]] = 1.0
+
+            self.forwardProp(trainImages[m].flatten())
+            self.backProp(trainImages[m].flatten(), correct_output)
+
+            if np.argmax(self.outputNeurons) == int(trainLabels[m]):
+                correct+=1
+
+            error = np.amax(np.absolute(self.outputLayerErrors))
+            err_sum += error
+            err_count += 1
+            avg_err = err_sum / err_count
+            left = self.width * int(m/(self.bs/100)) // 100
+            accuracy = str(int((correct/(m+1))*100)) + '%'
+            print (str(m) + '/' + str(self.bs) + ' ['+'='*left+">"+'.'*((self.width - left)-1)+']' + " - Accuracy: " + accuracy + " - Loss: " + str(round(avg_err, 10)), end="\r")
+        self.change()
+        batch_end_time = str(round(time.time() - batch_start_time, 2))
+        print (str(self.bs) + "/" + str(self.bs) + " ["+"="*self.width+"]" + " - " + batch_end_time + "s - Accuracy: " + accuracy + " - Loss: " + str(round(avg_err, 10)))
+        return avg_err
+
+    def predict(self, testImage):
+        self.forwardProp(testImage)
+        return np.argmax(self.outputNeurons)
+
+if __name__ == "__main__":
+
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    train_images = train_images/255
+
+    nn = NeuralNetwork([784, 128, 10], 'adam', 'relu')
+
+    start_time = time.time()
+    for i in range (nn.epochs):
+        start_time2 = time.time()
+        for j in range(int(nn.numUsing/nn.bs)):
+            print("Epoch", str(i+1) + "/" + str(nn.epochs) + ": Batch " + str(j+1) + "/" + str(int(nn.numUsing/nn.bs)))
+            nn.y[j+i*(int(nn.numUsing/nn.bs))] = nn.train(train_images[int(j * nn.bs):int(j * nn.bs) + nn.bs], train_labels[int(j * nn.bs):int(j * nn.bs) + nn.bs])
+        time2 = round((time.time() - start_time2), 2)
+        if time2 > 60:
+            print ("Epoch " + str(i+1) + " took " + str(round(time2/60, 2)) + " minutes\n\n")
+        else:
+            print ("Epoch " + str(i+1) + " took " + str(time2) + " seconds\n\n")
+    end_time = time.time() - start_time
+
+    print("\nTotal Time Used")
+    if end_time > 60:
+        print("Minutes: %s\n\n\n" % round((end_time/60),2))
+    else:
+        print("Seconds: %s\n\n\n" % round(end_time,2))
+
+    incorrect = 0
+    size = test_images.shape[0]
+    bar = IncrementalBar('TESTING:', max = size)
+    for i in range (size):
+        bar.next()
+        prediction = nn.predict(test_images[i].flatten())
+        correct = int(class_names[test_labels[i]])
+        if correct != prediction:
+            incorrect += 1
+    bar.finish()
+    print ("\nTESTING DONE\n" + "Correct: " + str(size-incorrect) + "\nIncorrect: " + str(incorrect) + "\n")
+
+    plt.plot(nn.x, nn.y)
+    plt.ylabel('Avg Error')
+    plt.xlabel('Batches')
+    plt.show()
+
+    stats_file = open("stats_file", "a+")
+    stats_file.write("Correct: " + str(size-incorrect) + "\nIncorrect: " + str(incorrect))
+    stats_file.write("\nOptimization: " + nn.optimizer + "\nActivation: " + nn.activation)
+    stats_file.write("\nNum Using: "+str(nn.numUsing) + "\nBatch Size: "+str(nn.bs) + "\nLearning Rate: "+str(nn.learningRate) + "\nLambda: "+str(nn.Lambda) + "\n\n\n")
+    stats_file.close
---
+++ SimpleNN.py
@@ -0,0 +1,166 @@
+import time
+import copy
+import matplotlib.pyplot as plt
+import numpy as np
+
+class NeuralNetwork():
+
+    width = 20
+    correct = 0
+    num_predictions = 10
+    learningRate = 0.1
+    beta = 0.9
+    Lambda = 0
+    epochs = 10000
+    correctTarget = 0.95
+
+    def __init__(self, sizes, sizeOfEpoch):
+        self.sizeOfEpoch = sizeOfEpoch 
+        self.dimensions = sizes
+
+        self.x = np.arange(1,self.epochs+1)
+        self.accuracyY = np.empty(self.epochs)
+        self.errorY = np.empty(self.epochs)
+
+        self.secondLayerNeurons = np.empty(sizes[1])
+        self.outputNeurons = np.empty(sizes[2])
+
+        self.firstLayerWeights = np.random.rand(sizes[1], sizes[0]) * 2 - 1
+        self.secondLayerWeights = np.random.rand(sizes[2], sizes[1]) * 2 - 1
+        self.firstLayerBiases = np.random.rand(sizes[1]) * 2 - 1
+        self.secondLayerBiases = np.random.rand(sizes[2]) * 2 - 1
+
+        self.firstLayerWeightsSummations = np.zeros([sizes[1], sizes[0]])
+        self.secondLayerWeightsSummations = np.zeros([sizes[2], sizes[1]])
+        self.firstLayerBiasesSummations = np.zeros([sizes[1]])
+        self.secondLayerBiasesSummations = np.zeros([sizes[2]])
+
+        self.firstLayerWeightsVelo = np.zeros([sizes[1], sizes[0]])
+        self.secondLayerWeightsVelo = np.zeros([sizes[2], sizes[1]])
+        self.firstLayerBiasesVelo = np.zeros([sizes[1]])
+        self.secondLayerBiasesVelo = np.zeros([sizes[2]])
+
+        self.hiddenLayerErrors = np.empty(sizes[1])
+        self.outputLayerErrors = np.empty(sizes[2])
+        self.greatestError = np.zeros(1)
+
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+
+    def sigmoidDerivative(self, x):
+        return np.multiply(x,(1-x))
+
+    def forwardProp(self, inputs):
+        for i in range (self.dimensions[1]):
+            self.secondLayerNeurons[i] = self.sigmoid(np.dot(self.firstLayerWeights[i], inputs)+self.firstLayerBiases[i])
+        for i in range (self.dimensions[2]):
+            self.outputNeurons[i] = self.sigmoid(np.dot(self.secondLayerWeights[i], self.secondLayerNeurons)+self.secondLayerBiases[i])
+
+    def backProp(self, inputs, correct_output):
+        self.outputLayerErrors = np.subtract(self.outputNeurons, correct_output)
+        self.hiddenLayerErrors = np.multiply(np.dot(self.secondLayerWeights.T, self.outputLayerErrors), self.sigmoidDerivative(self.secondLayerNeurons))
+
+        self.secondLayerBiasesSummations += self.outputLayerErrors
+        self.secondLayerWeightsSummations += np.outer(self.outputLayerErrors, self.secondLayerNeurons)
+
+        self.firstLayerBiasesSummations += self.hiddenLayerErrors
+        self.firstLayerWeightsSummations += np.outer(self.hiddenLayerErrors, inputs)
+
+    def change(self):
+        self.secondLayerBiasesVelo = self.beta*self.secondLayerBiasesVelo + (1-self.beta)*self.secondLayerBiasesSummations
+        self.secondLayerWeightsVelo = self.beta*self.secondLayerWeightsVelo + (1-self.beta)*self.secondLayerWeightsSummations
+        self.firstLayerBiasesVelo = self.beta*self.firstLayerBiasesVelo + (1-self.beta)*self.firstLayerBiasesSummations
+        self.firstLayerWeightsVelo = self.beta*self.firstLayerWeightsVelo + (1-self.beta)*self.firstLayerWeightsSummations
+
+        self.secondLayerBiases -= self.learningRate * self.secondLayerBiasesVelo
+        self.secondLayerWeights -= self.learningRate * (self.secondLayerWeightsVelo + self.Lambda * self.secondLayerWeights)
+        self.firstLayerBiases -= self.learningRate * self.firstLayerBiasesVelo
+        self.firstLayerWeights -= self.learningRate * (self.firstLayerWeightsVelo + self.Lambda * self.firstLayerWeights)
+
+        self.firstLayerWeightsSummations = np.zeros([self.dimensions[1], self.dimensions[0]])
+        self.secondLayerWeightsSummations = np.zeros([self.dimensions[2], self.dimensions[1]])
+        self.firstLayerBiasesSummations = np.zeros(self.dimensions[1])
+        self.secondLayerBiasesSummations = np.zeros(self.dimensions[2])
+
+    def train(self, trainImages, trainLabels):
+        size = str(self.sizeOfEpoch)
+        start_time2 = time.time()
+
+        for m in range (self.sizeOfEpoch):
+            correct_output = trainLabels[m]
+
+            self.forwardProp(trainImages[m].flatten())
+            self.backProp(trainImages[m].flatten(), correct_output)
+
+            if np.absolute(self.outputLayerErrors) > self.greatestError:
+                self.greatestError = np.absolute(self.outputLayerErrors)
+            if self.outputNeurons > self.correctTarget and trainLabels[m] == 1 or self.outputNeurons < 0.1 and trainLabels[m] == 0:
+                self.correct+=1
+            accuracy = str(int((self.correct/(m+1))*100)) + '%'
+            percent = str(int((m/self.sizeOfEpoch)*100)) + '%'
+            print ("Progress: " + percent + " -- Accuracy: " + accuracy, end="\r")
+        self.change()
+
+        time2 = str(round((time.time() - start_time2), 2))
+        print (size+'/'+size+" -- "+time2+"s"+" -- Accuracy: "+accuracy+" -- Error:",self.greatestError,end="\r")
+        correct = np.copy(self.correct)
+        self.correct = 0
+        return (correct/self.sizeOfEpoch)
+            
+    def predict(self, testImage):
+        self.forwardProp(testImage)
+        return self.outputNeurons
+
+if __name__ == "__main__":
+
+    train_images = np.array([[1,0,0,1],
+                            [1,0,1,0],
+                            [0,1,0,1],
+                            [0,0,0,0],
+                            [1,1,1,1]])
+    train_labels = np.array([1, 0, 0, 0, 0])
+    # train_images = np.array([[0,0], [0,1], [1,0], [1,1]])
+    # train_labels = np.array([0,1,1,0])
+
+    neural_network = NeuralNetwork([4, 3, 1], train_images.shape[0])
+
+    start_time = time.time()
+    for i in range (neural_network.epochs):
+        print ("\nEpoch", str(i+1) + "/" + str(neural_network.epochs))
+        neural_network.accuracyY[i]=neural_network.train(train_images, train_labels)
+        neural_network.errorY[i] = np.copy(neural_network.greatestError)
+        neural_network.greatestError = 0
+    time = time.time() - start_time
+
+    plt.plot(neural_network.x, neural_network.errorY, 'r')
+    plt.plot(neural_network.x, neural_network.accuracyY, 'b')
+    plt.ylabel('Change In Error/Accuaracy')
+    plt.xlabel('Epochs')
+    plt.show()
+
+    print("\n\n\nTotal Time Used")
+    if time > 60:
+        print("Minutes: %s" % round((time/60),2))
+    else:
+        print("Seconds: %s" % round(time,2))
+
+    for i in range (neural_network.num_predictions):
+        print("\n\nNew Situations: " + str(i+1) + "/" + str(neural_network.num_predictions))
+        A = list(map(int,input("Enter the numbers : ").strip().split()))[:4] 
+   
+        try:
+            result = neural_network.predict(A)
+        except:
+            print("\nError, try again")
+            continue
+
+        print("\nOutput Data:", result)
+       
+        if result>neural_network.correctTarget:
+            print("Result: Back Slash")
+        else:
+            print("Result: Not Back Slash")
+        # if result>0.95:
+        #     print("Result: 1")
+        # else:
+        #     print("Result: 0")
